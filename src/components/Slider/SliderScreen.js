e5d6df823a9fcba18754a69c678d9e2385737382
krtya.admin <krtya.admin@krtyasoftwares.net>
2020-12-11 10:38:08 +0530

    UPDATE

--- src/components/Slider/SliderScreen.js
+++ src/components/Slider/SliderScreen.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { SliderBox } from 'react-native-image-slider-box';
-import * as image from '../Slider/SliderImage'
 import {
     heightPercentageToDP as hp,
     widthPercentageToDP as wp,
@@ -8,16 +7,15 @@ import {
 
 export default function SliderScreen() {
     const images = [
-        image.sliderimg1,
-        image.sliderimg2,
-        image.sliderimg3,
-        image.sliderimg4
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm_mx0EvyRIh06PAlNfe4vFqKH5135P99GZw&usqp=CAU',
+        'https://spin-caps.com/wp-content/uploads/2018/09/car-service-key-features-A-1920x1080_tcm-3154-1323224.jpg',
+        'https://pictures.dealer.com/u/universalcitynissanatadgcompany/1832/9ba83c80d165a23e0dd87d858752e4b7x.jpg?impolicy=downsize&w=568',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGVnUl7MFFwUwVp7YEqN-RlHcd3HAwalJvAQ&usqp=CAU'
     ]
     return (
         <SliderBox
             images={images}
             sliderBoxHeight={200}
-            //dotColor="#2f408f"
             inactiveDotColor="#90A4AE"
             paginationBoxVerticalPadding={0}
             autoplay
