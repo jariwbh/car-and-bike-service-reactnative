be44f482b7bf5e246f47db958e66f14d01fb8f42
HarshadJariwala <harshadjariwala1998@gmail.com>
2021-01-19 12:50:37 +0530

    update

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import NavigationApp from './src/navigations/NavigationsApp';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync()
+  setTimeout(async () => {
+    await SplashScreen.hideAsync();
+  }, 2000);
   return (
     <NavigationApp />
   );
